452b43872a175fdeb78c11708dc21e5a6819f985
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-03-16 23:35:12 +0200

    Implement simple shader class

---
+++ OpenGLTechTalks/Basic/src/Shader.cpp
@@ -0,0 +1,128 @@
+#include "shader.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+
+namespace core
+{
+    Shader::Shader() : m_ID{}
+    {
+    }
+
+    Shader::Shader(const char* vertexPath, const char* fragmentPath) : Shader()
+    {
+        // 1. retrieve the vertex/fragment source code from filePath
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+        try
+        {
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            vShaderFile.close();
+            fShaderFile.close();
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        }
+        catch (const std::ifstream::failure&)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ::" << std::endl;
+        }
+
+        // 2. compile shaders
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        unsigned int vertex, fragment;
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, nullptr);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, nullptr);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+
+        m_ID = glCreateProgram();
+        glAttachShader(m_ID, vertex);
+        glAttachShader(m_ID, fragment);
+        glLinkProgram(m_ID);
+        checkCompileErrors(m_ID, "PROGRAM");
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+    Shader::~Shader()
+    {
+        if (m_ID == 0) return;
+
+        glDeleteProgram(m_ID);
+    }
+
+    void Shader::activate()
+    {
+        glUseProgram(m_ID);
+    }
+
+    void Shader::deActivate()
+    {
+        glUseProgram(0);
+    }
+
+    unsigned int Shader::getID()
+    {
+        return m_ID;
+    }
+
+    void Shader::setBool(const std::string& name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(m_ID, name.c_str()), (int)value);
+    }
+
+    void Shader::setInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(m_ID, name.c_str()), value);
+    }
+
+    void Shader::setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(m_ID, name.c_str()), value);
+    }
+
+    void Shader::checkCompileErrors(GLuint shader, std::string type)
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, nullptr, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
+}
---
+++ OpenGLTechTalks/Basic/src/Shader.h
@@ -0,0 +1,38 @@
+#ifndef SHADER
+#define SHADER
+
+#include <glad/glad.h>
+
+#include <string>
+
+namespace core
+{
+    class Shader
+    {
+    private:
+        unsigned int m_ID;
+    public:
+        Shader();
+
+        Shader(const char* vertexPath, const char* fragmentPath);
+
+        ~Shader();
+
+        void activate();
+
+        void deActivate();
+
+        unsigned int getID();
+
+        void setBool(const std::string& name, bool value) const;
+
+        void setInt(const std::string& name, int value) const;
+
+        void setFloat(const std::string& name, float value) const;
+
+    private:
+        void checkCompileErrors(GLuint shader, std::string type);
+    };    
+}
+
+#endif
