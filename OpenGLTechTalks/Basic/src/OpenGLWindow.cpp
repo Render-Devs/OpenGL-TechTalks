c6529073308220e83147f633f9c3824ab23fcfc8
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-05-16 22:56:35 +0300

    Implement rendering to framebuffer

--- OpenGLTechTalks/Basic/src/OpenGLWindow.cpp
+++ OpenGLTechTalks/Basic/src/OpenGLWindow.cpp
@@ -42,6 +42,7 @@ void OpenGLWindow::Init(const WindowData& data)
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	m_Window = glfwCreateWindow(data.width, data.height, m_Data.Title.c_str(), nullptr, nullptr);
 	m_Context = CreateScope<OpenGLContext>(m_Window);
 	m_Context->Init();
--- OpenGLTechTalks/SimpleMultisampling/src/main.cpp
+++ OpenGLTechTalks/SimpleMultisampling/src/main.cpp
@@ -30,32 +30,71 @@ private:
          0.0f, -0.5f, Z_SECOND,
     };
 
+    const std::vector<GLfloat> quadVertices
+    {    
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    uint32_t width{ 800 };
+    uint32_t height{ 600 };
+
     GLuint vao{};
     GLuint vbo{};
     core::Shader* program{};
 
+    GLuint quadVao{};
+    GLuint quadVbo{};
+    core::Shader* screenProgram{};
+
+    GLuint fbo{};
+    GLuint texColorBuffer{};
+    GLuint rbo{};
+
 protected:
     void Init() override
     {
+        InitFramebuffer();
         InitBuffers();
 
         program = new core::Shader("vertex.vert", "fragment.frag");
+        screenProgram = new core::Shader("quad.vert", "quad.frag");
+        screenProgram->SetActive(true);
+        screenProgram->setInt("screenTexture", 0);
     }
 
     void Update(float deltaTime) override
     {
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+        glEnable(GL_DEPTH_TEST);
         glClearColor(COLOR_YELLOW.x, COLOR_YELLOW.y, COLOR_YELLOW.z, COLOR_YELLOW.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBindVertexArray(vao);
-
         program->SetActive(true);
 
         program->setVec4(SHADER_PROPERTY_COLOR, COLOR_BLUE);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         program->setVec4(SHADER_PROPERTY_COLOR, COLOR_RED);
-        glDrawArrays(GL_TRIANGLES, 3, 6);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDisable(GL_DEPTH_TEST);
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        screenProgram->SetActive(true);
+        glBindVertexArray(quadVao);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
     }
 
     void PostUpdate(float) override
@@ -63,14 +102,20 @@ protected:
     }
 
     void Dispose() override
-    {
-        glDeleteVertexArrays(1, &vao);
+    {        
         glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        glDeleteRenderbuffers(1, &rbo);
+        glDeleteFramebuffers(1, &fbo);
+
+        delete screenProgram;
         delete program;
     }
 
     void SetViewport(uint32_t width, uint32_t height) override
     {
+        this->width = width;
+        this->height = height;
         glViewport(0, 0, width, height);
     }
 
@@ -90,7 +135,51 @@ private:
 
         glBindVertexArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+        glGenVertexArrays(1, &quadVao);
+        glGenBuffers(1, &quadVbo);
+
+        glBindVertexArray(quadVao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, quadVbo);
+        glBufferData(GL_ARRAY_BUFFER, quadVertices.size() * sizeof(GLfloat), quadVertices.data(), GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
+
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void InitFramebuffer()
+    {
+        glGenFramebuffers(1, &fbo);
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        
+        glGenTextures(1, &texColorBuffer);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+
+
+        glGenRenderbuffers(1, &rbo);
+        glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
 };
 
